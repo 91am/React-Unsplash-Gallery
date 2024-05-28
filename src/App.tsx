5838727b5c9764bcb9c35b01a53e3f23ef1271fe
91am <killaek.info@gmail.com>
2024-05-28 20:19:46 +0200

    add search input

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,24 @@ function App() {
     <main className="flex w-screen h-screen justify-center">
       <div className="max-w-7xl">
         <h1 className="text-7xl">Unsplash Gallery</h1>
+        <div className='flex'>
+          <label className="input input-bordered flex items-center gap-2">
+            <input type="text" className="grow" placeholder="Search" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </label>
+          <button className="btn">Button</button>
+        </div>
         <div className="flex flex-wrap gap-8">
           <Card></Card>
           <Card></Card>
