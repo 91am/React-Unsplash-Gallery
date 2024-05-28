341fa10904f6653b1e6a381d3534d91ae96965dd
91am <killaek.info@gmail.com>
2024-05-28 20:10:47 +0200

    add Daisy and Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from "./components/Card"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex w-screen h-screen justify-center">
+      <div className="max-w-7xl">
+        <h1 className="text-7xl">Unsplash Gallery</h1>
+        <div className="flex flex-wrap gap-8">
+          <Card></Card>
+          <Card></Card>
+          <Card></Card>
+        </div>
       </div>
-      <h1>Unsplash Gallery</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
 export default App
